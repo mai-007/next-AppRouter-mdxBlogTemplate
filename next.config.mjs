ade61865914faf07b3c34a494046ce36e00637f9
mai kotou <mai@mainoMacBook-Pro.local>
2024-01-16 20:07:03 +0900

    add toc

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,19 @@ import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypePrism from "@mapbox/rehype-prism";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
 
 const withMDX = nextMDX({
   extensions: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [remarkGfm, remarkMath],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [
+      remarkGfm,
+      remarkMath,
+      [remarkToc, { maxDepth: 3, heading: "目次" }],
+    ],
+    rehypePlugins: [rehypeKatex, rehypePrism, rehypeSlug],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },

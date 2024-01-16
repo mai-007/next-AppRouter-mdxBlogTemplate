fa3c022cf690ad319ab34ca6d40b98339e78cbf9
mai kotou <mai@mainoMacBook-Pro.local>
2024-01-16 16:54:24 +0900

    add TeX

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const withMDX = nextMDX({
   extensions: /\.mdx?$/,
@@ -9,8 +11,8 @@ const withMDX = nextMDX({
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
